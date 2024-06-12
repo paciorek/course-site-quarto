10e41ef94b53db01bf96e8dadab22628c5eb1b35
Christopher Paciorek <paciorek@stat.berkeley.edu>
2024-06-12 16:13:17 -0700

    add extension files

---
+++ _extensions/qmd-lab/scheduled-docs/run-scheduled-docs.ts
@@ -0,0 +1,19 @@
+
+// Import external libraries
+import { readYML, propagateKeys, processSchedule, writeDraftList, writeScheduledDocs, writeListingContents } from "./scheduled-docs.ts";
+
+// Set parameters
+const ymlPath = 'schedule.yml';
+const scheduledDocsKey = 'scheduled-docs';
+const itemsKey = 'docs';
+const tempFilesDir = './scheduled-docs_files';
+const tempFilesDirOffset = "..";
+
+// Run functions
+console.log("=== Scheduled-docs ===")
+let scheduledDocs = await readYML(ymlPath, scheduledDocsKey);
+propagateKeys(scheduledDocs);
+processSchedule(scheduledDocs, itemsKey);
+await writeDraftList(scheduledDocs, tempFilesDir);
+await writeScheduledDocs(scheduledDocs, tempFilesDir);
+await writeListingContents(scheduledDocs, tempFilesDir);
